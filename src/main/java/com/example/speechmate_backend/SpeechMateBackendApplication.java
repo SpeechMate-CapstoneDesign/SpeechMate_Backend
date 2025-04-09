234904931adf5847e3aec727805f5c095e046825
jeongminkang <wjdals980424@naver.com>
2025-04-09 13:24:51 +0900

    first commit

---
+++ src/main/java/com/example/speechmate_backend/SpeechMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.speechmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechMateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeechMateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/speechmate_backend/SpeechMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speechmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechMateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

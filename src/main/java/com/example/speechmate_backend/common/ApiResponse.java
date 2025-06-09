b2327f42e947401f37095c95ee727a49aab356aa
jeongminkang <wjdals980424@naver.com>
2025-06-10 02:04:56 +0900

    feat: 예외 처리및 공통 응답

--- src/main/java/com/example/speechmate_backend/common/ApiResponse.java
+++ src/main/java/com/example/speechmate_backend/common/ApiResponse.java
@@ -1,8 +1,10 @@
 package com.example.speechmate_backend.common;
 
+import com.example.speechmate_backend.common.error.ErrorCodeIfs;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
@@ -13,5 +15,16 @@ public class ApiResponse<T>{
     private Integer resultCode;
     private T data;
 
+    public static <T> ApiResponse<T> ok(T data){
+        return new ApiResponse<>("success", HttpStatus.OK.value(), data);
+    }
+
+    public static ApiResponse<String> fail(ErrorCodeIfs errorCode){
+        return new ApiResponse<>(errorCode.getStatus(), errorCode.getResultCode(), errorCode.getMessage());
+    }
+
+    public static <T> ApiResponse<T> fail(String status, Integer code, T data){
+        return new ApiResponse<>(status, code, data);
+    }
 
 }
--- src/main/java/com/example/speechmate_backend/common/error/ErrorCode.java
+++ src/main/java/com/example/speechmate_backend/common/error/ErrorCode.java
@@ -7,8 +7,12 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode implements ErrorCodeIfs{
 
-    INVALID_TOKEN("fail", 401,"잘못된 토큰, 재 로그인해주세요"),
-    TOKEN_EXPIRED("fail", 401, "만료된 토큰");
+    INVALID_TOKEN("fail", 101,"잘못된 토큰, 재 로그인해주세요"),
+    TOKEN_EXPIRED("fail", 102, "만료된 토큰"),
+    OAUTH_PROVIDER_NOT_MATCH("fail", 103, "provider가 올바르지 않음"),
+    USER_ALREADY_EXIST_INFO("fail", 104, "이미 존재하는 Oauth 정보"),
+    ;
+
 
     private final String status;
     private final Integer resultCode;

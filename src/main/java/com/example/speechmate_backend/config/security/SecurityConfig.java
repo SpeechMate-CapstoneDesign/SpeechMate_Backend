f283c0f896bd22a91cbf5e63e1fd2c0fdc2bb05b
jeongminkang <wjdals980424@naver.com>
2026-03-15 17:44:08 +0900

    chore: import 제거

--- src/main/java/com/example/speechmate_backend/config/security/SecurityConfig.java
+++ src/main/java/com/example/speechmate_backend/config/security/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
 @EnableWebSecurity
